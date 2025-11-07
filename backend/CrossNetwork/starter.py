bab11c74f53e11795b36855661f44928ba123611
Samarth Naik <naikm.sam@gmail.com>
2025-11-08 00:26:03 +0530

    added test data folder

--- backend/CrossNetwork/starter.py
+++
@@ -1,9 +0,0 @@
-from nacl import public
-
-sk = public.PrivateKey.generate()
-pk = sk.public_key
-
-with open("receiver_secret.key", "wb") as f:
-    f.write(sk.encode())
-with open("receiver_public.key", "wb") as f:
-    f.write(pk.encode())
--- backend/connector.py
+++
@@ -1,3 +0,0 @@
-#This file will be used to connect the backend with the frontend.
-#The front end will not have to view/check any other file except this
-
